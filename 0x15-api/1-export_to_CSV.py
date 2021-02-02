a421b20dc469aa646a938c3cd811fbd67e7900d0
KarenCampo777 <1472@holbertonschool.com>
2021-02-02 04:23:40 +0000

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" CSV  Module
+"""
+import requests
+import csv
+from sys import argv as av
+
+
+if __name__ == '__main__':
+    api = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(api + 'users/{}'.format(av[1])).json()
+    todo = requests.get(api + 'users/{}/todos'.format(av[1])).json()
+    row = [
+        [
+            av[1],
+            user['username'],
+            task.get('completed'),
+            task.get('title')
+        ] for task in todo
+    ]
+    file = '{}.csv'.format(av[1])
+    with open(file, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerows(row)
