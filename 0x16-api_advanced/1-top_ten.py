c59f693084a18bdddbf74bf20f061ed35ffcc757
KarenCampo777 <1472@holbertonschool.com>
2021-02-05 03:45:18 +0000

    adding task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints
+the titles of the first 10 hot posts listed for a given subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    try:
+        rq = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'.
+                          format(subreddit),
+                          headers={'User-Agent': 'post'},
+                          allow_redirects=False)
+        for thread in rq.json().get('data').get('children'):
+            print(thread.get('data').get('title'))
+    except Exception:
+        return None
